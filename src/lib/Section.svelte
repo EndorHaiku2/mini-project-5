b93f0a237c57027a4043e8e5fd50811980f0dfa8
EndorHaiku2 <clannerconner55@gmail.com>
2021-11-07 06:49:53 +0000

    a

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -3,6 +3,14 @@
     export let item1
     export let item2
     export let item3
+    export let title1
+    export let item4
+    export let item5
+    export let item6
+    export let title2
+    export let item7
+    export let item8
+    export let item9
 </script>
     <div>
         <h3>{title}</h3>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,8 +5,18 @@
      title: "Info",
    	 item1: "LZY",
    	 item2: 14,
-   	 item3: "Telebort",}
+   	 item3: "Telebort",
+
+     title1: "Hobby",
+     item4: "Drawing",
+     item5: "Cooking",
+     item6: "Badminton",
+
+     title2 "Personality",
+     item7: "Loud",
+     item8: "Depends",
+     item9: "Kind"}
 </script>
- 
-<Header title={"My Biography"}/>
+
+<Header title={'My Biography'} />
 <Section {...myInfo} />
