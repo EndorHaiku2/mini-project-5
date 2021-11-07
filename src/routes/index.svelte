d1ca41ce11b81b62af5f0d6f0fb6c1351d277367
EndorHaiku2 <clannerconner55@gmail.com>
2021-11-07 06:39:49 +0000

    a

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,12 @@
 <script>
     import Header from "$lib/Header.svelte";
+    import Section from "$lib/Section.svelte";
+    let myInfo = {
+     title: "Info",
+   	 item1: "LZY",
+   	 item2: 14,
+   	 item3: "Telebort",}
 </script>
-
+ 
 <Header title={"My Biography"}/>
+<Section {"...myInfo"} />
