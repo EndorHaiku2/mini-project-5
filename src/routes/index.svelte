580f309ec55212a3c67d7f7f9c26508be450f236
EndorHaiku2 <clannerconner55@gmail.com>
2021-11-07 06:59:54 +0000

    as

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,20 @@
     let myInfo = {
      title: "Info",
    	 item1: "LZY",
-   	 item2: 14,
+   	 item2: "14",
    	 item3: "Telebort"}
+
+    let myHobby = {
+     title: "Hobby",
+   	 item1: "Drawing",
+   	 item2: "Badminton",
+   	 item3: "More drawing"}
+
+    let myPersonality = {
+     title: "Personality",
+   	 item1: "Loud",
+   	 item2:  "Happy",
+   	 item3: "Usually carefree"}      
 </script>
 
 <Header title={'My Biography'} />
